4cac8a90163d2c00ab5302fc226577cee2082923
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2022-11-15 23:14:47 -0300

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './index.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Wolf Tattoo Studio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Agendamentos</Nav.Link>
+            <Nav.Link href="#pricing">Equipe</Nav.Link>
+            <NavDropdown title="Tatuadores" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Tatuador 1</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Tatuador 2
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Tatuador 3</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
+
 
 export default App
